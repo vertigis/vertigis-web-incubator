74e922e3e13a47feb722e0a4ccb1b8f6dc1333a9
Scott Anderson <scott.anderson@vertigis.com>
2025-07-04 13:38:40 -0700

    updates

--- libraries/eagle-view-viewer/src/components/EmbeddedExplorer/EagleView.tsx
+++ libraries/eagle-view-viewer/src/components/EmbeddedExplorer/EagleView.tsx
@@ -6,6 +6,8 @@ import ReactDOM from "react-dom";
 import type { EagleViewModel } from ".";
 import { createEagleViewScript, type WindowWithEv } from "./embedded-explorer";
 
+const selectorsToTrim = ["h1", "h2", "h3", "h4", "h5", "h6", "body", "label"];
+
 export default function EagleView(
     props: LayoutElementProperties<EagleViewModel>
 ): React.ReactElement {
@@ -15,14 +17,41 @@ export default function EagleView(
         const window = document.defaultView as WindowWithEv;
         const mountEmbeddedExplorer = () => {
             const explorer = new window.ev.EmbeddedExplorer();
+
             model.e3 = explorer.mount("eagle-view-map", {
                 apiKey: model.apiKey,
                 view: model.getPointForEagleView(),
             });
+
+            /**
+             * EagleView pollutes the page with inline styles that override
+             * global styles. This is a workaround to remove those styles.
+             */
+            const newStyles = [...document.styleSheets].filter(
+                (style) => !previousStyle.includes(style)
+            );
+            newStyles.forEach((style) => {
+                // Remove inline styles that are not from the EagleView library.
+                const rules = [...style.cssRules];
+                for (let index = rules.length - 1; index >= 0; index--) {
+                    const rule = rules[index];
+                    if (
+                        rule instanceof CSSStyleRule &&
+                        selectorsToTrim.some(
+                            (selector) => rule.selectorText === selector
+                        )
+                    ) {
+                        console.error(
+                            `Removing inline style rule: ${rule.selectorText}`
+                        );
+                        style.deleteRule(index);
+                    }
+                }
+            });
         };
 
         const evScriptElt = document.getElementById("embedded-explorer-widget");
-
+        const previousStyle = [...document.styleSheets];
         if (evScriptElt == null) {
             // This is required to get the EagleView script running.
             window.define("React", React);
